1ea95a4093ffa5fa0f283df7b0abe68b1ac142f1
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2022-11-06 19:20:46 +0900

    add functions `create/2`, `has_path/3` and their tests

--- src/core/my_digraph.erl
+++ src/core/my_digraph.erl
@@ -1,6 +1,6 @@
 -module(my_digraph).
 
--export([union/3, get_longest_straight_path/2]).
+-export([union/3, get_longest_straight_path/2, create/2, has_path/3]).
 
 -spec union(digraph:graph(), [digraph:vertex()], digraph:vertex()) -> digraph:vertex().
 union(Graph, Vertices, NewVertex) ->
@@ -50,4 +50,20 @@ get_longest_straight_path_aux(Graph, Vertex) ->
                 _ -> []
             end;
         false -> []
+    end.
+
+% TODO: Support a labeled vertex and a labeled edge
+-spec create([digraph:vertex()], [{digraph:vertex(), digraph:vertex()}]) ->
+                digraph:graph().
+create(Vertices, Edges) ->
+    G = digraph:new(),
+    lists:foreach(fun(X) -> digraph:add_vertex(G, X) end, Vertices),
+    lists:foreach(fun({V1, V2}) -> digraph:add_edge(G, V1, V2) end, Edges),
+    G.
+
+-spec has_path(digraph:graph(), digraph:vertex(), digraph:graph()) -> boolean().
+has_path(G, V1, V2) ->
+    case digraph:get_path(G, V1, V2) of
+        false -> false;
+        _ -> true
     end.
