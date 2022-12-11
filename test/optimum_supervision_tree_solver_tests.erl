d808ccd63947ed7e1796fe77311da5c1cfed2440
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2022-12-11 17:20:08 +0900

    fix the order of supervisor's child processes

--- test/optimum_supervision_tree_solver_tests.erl
+++ test/optimum_supervision_tree_solver_tests.erl
@@ -2,6 +2,8 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+-import(lists, [map/2, sort/1]).
+
 % NOTE:
 % Only test for types,
 % since `group/1` and `transform_into_optimum_supervision_tree/1` tests are enriched.
@@ -17,7 +19,7 @@ group_test_() ->
         fun(Graph) ->
            lists:map(fun(E) ->
                         {E, V1, V2, _} = digraph:edge(Graph, E),
-                        {V1, V2}
+                        {sort(V1), sort(V2)}
                      end,
                      digraph:edges(Graph))
         end,
@@ -34,10 +36,10 @@ group_test_() ->
           lists:foreach(fun(X) -> digraph:add_vertex(G, X) end, lists:seq(1, 7)),
           lists:foreach(fun({V1, V2}) -> digraph:add_edge(G, V1, V2) end,
                         [{1, 3}, {2, 3}, {3, 4}, {3, 5}, {6, 7}]),
-          ?assertEqual(lists:sort([[1], [2], [3], [4, 5], [6], [7]]),
-                       lists:sort(
-                           digraph:vertices(
-                               optimum_supervision_tree_solver:group(G))))
+          ?assertEqual(sort(map(fun lists:sort/1, [[1], [2], [3], [4, 5], [6], [7]])),
+                       sort(map(fun lists:sort/1,
+                                digraph:vertices(
+                                    optimum_supervision_tree_solver:group(G)))))
        end},
       {"Keep the original edges",
        fun() ->
@@ -46,13 +48,8 @@ group_test_() ->
           lists:foreach(fun({V1, V2}) -> digraph:add_edge(G, V1, V2) end,
                         [{1, 3}, {2, 3}, {3, 4}, {3, 5}, {6, 7}]),
           GroupedGraph = optimum_supervision_tree_solver:group(G),
-          ?assertEqual(lists:sort([{[1], [3]}, {[2], [3]}, {[3], [4, 5]}, {[6], [7]}]),
-                       lists:sort(
-                           lists:map(fun(E) ->
-                                        {E, V1, V2, _} = digraph:edge(GroupedGraph, E),
-                                        {V1, V2}
-                                     end,
-                                     digraph:edges(GroupedGraph))))
+          ?assertEqual(sort([{[1], [3]}, {[2], [3]}, {[3], sort([4, 5])}, {[6], [7]}]),
+                       sort(ExtractEdges(GroupedGraph)))
        end},
       {"Can group a graph like a lattice",
        fun() ->
@@ -61,10 +58,11 @@ group_test_() ->
           lists:foreach(fun({V1, V2}) -> digraph:add_edge(G, V1, V2) end,
                         [{1, 3}, {1, 4}, {2, 4}, {2, 5}, {3, 6}, {4, 6}, {5, 6}]),
           GroupedGraph = optimum_supervision_tree_solver:group(G),
-          ?assertEqual(lists:sort([[6], [3, 4, 5], [1], [2]]),
-                       lists:sort(
-                           digraph:vertices(GroupedGraph))),
-          ?assertEqual(lists:sort([{[1], [3, 4, 5]}, {[2], [3, 4, 5]}, {[3, 4, 5], [6]}]),
+          ?assertEqual(sort(map(fun lists:sort/1, [[6], [3, 4, 5], [1], [2]])),
+                       sort(map(fun lists:sort/1, digraph:vertices(GroupedGraph)))),
+          ?assertEqual(lists:sort([{[1], sort([3, 4, 5])},
+                                   {[2], sort([3, 4, 5])},
+                                   {sort([3, 4, 5]), [6]}]),
                        lists:sort(ExtractEdges(GroupedGraph)))
        end},
       {"Can group a graph like a binary tree",
@@ -74,26 +72,25 @@ group_test_() ->
           lists:foreach(fun({V1, V2}) -> digraph:add_edge(G, V1, V2) end,
                         [{1, 3}, {2, 3}, {3, 7}, {4, 6}, {5, 6}, {6, 7}]),
           GroupedGraph = optimum_supervision_tree_solver:group(G),
-          ?assertEqual(lists:sort([[1], [2], [3], [4], [5], [6], [7]]),
-                       lists:sort(
-                           digraph:vertices(GroupedGraph))),
-          ?assertEqual(lists:sort([{[1], [3]},
-                                   {[2], [3]},
-                                   {[3], [7]},
-                                   {[4], [6]},
-                                   {[5], [6]},
-                                   {[6], [7]}]),
-                       lists:sort(ExtractEdges(GroupedGraph)))
+          ?assertEqual(sort([[1], [2], [3], [4], [5], [6], [7]]),
+                       sort(digraph:vertices(GroupedGraph))),
+          ?assertEqual(sort([{[1], [3]},
+                             {[2], [3]},
+                             {[3], [7]},
+                             {[4], [6]},
+                             {[5], [6]},
+                             {[6], [7]}]),
+                       sort(ExtractEdges(GroupedGraph)))
        end},
       {"Can group a graph having a circle",
        fun() ->
           G = my_digraph:create(
                   lists:seq(1, 5), [{1, 2}, {2, 3}, {3, 4}, {4, 2}, {3, 5}]),
           GroupedGraph = optimum_supervision_tree_solver:group(G),
-          ?assertEqual(lists:sort([[1], [2, 3, 4], [5]]),
-                       lists:sort(
-                           digraph:vertices(GroupedGraph))),
-          ?assertEqual(lists:sort([{[1], [2, 3, 4]}, {[2, 3, 4], [5]}]), ExtractEdges(GroupedGraph))
+          ?assertEqual(sort(map(fun lists:sort/1, [[1], [2, 3, 4], [5]])),
+                       sort(map(fun lists:sort/1, digraph:vertices(GroupedGraph)))),
+          ?assertEqual(sort([{[1], sort([2, 3, 4])}, {sort([2, 3, 4]), [5]}]),
+                       ExtractEdges(GroupedGraph))
        end},
       {"Can group a graph having circles in first and last",
        fun() ->
@@ -101,18 +98,17 @@ group_test_() ->
               optimum_supervision_tree_solver:group(
                   my_digraph:create(
                       lists:seq(1, 6), [{1, 2}, {2, 3}, {3, 1}, {3, 4}, {4, 5}, {5, 6}, {6, 5}])),
-          ?assertEqual(lists:sort([[1, 2, 3], [4], [5, 6]]),
-                       lists:sort(
-                           digraph:vertices(GroupedGraph))),
-          ?assertEqual(lists:sort([{[1, 2, 3], [4]}, {[4], [5, 6]}]),
-                       lists:sort(ExtractEdges(GroupedGraph)))
+          ?assertEqual(sort(map(fun lists:sort/1, [[1, 2, 3], [4], [5, 6]])),
+                       sort(map(fun lists:sort/1, digraph:vertices(GroupedGraph)))),
+          ?assertEqual(sort([{sort([1, 2, 3]), [4]}, {[4], sort([5, 6])}]),
+                       sort(ExtractEdges(GroupedGraph)))
        end},
       {"Can group a graph having strongly connected components",
        fun() ->
           GroupedGraph =
               optimum_supervision_tree_solver:group(
                   my_digraph:create([1, 2, 3, 4], [{1, 2}, {2, 3}, {2, 4}, {3, 1}, {4, 1}])),
-          ?assertEqual([[1, 2, 3, 4]], digraph:vertices(GroupedGraph)),
+          ?assertEqual([sort([1, 2, 3, 4])], map(fun lists:sort/1, digraph:vertices(GroupedGraph))),
           ?assertEqual([], ExtractEdges(GroupedGraph))
        end},
       {"Can group a complex graph",
@@ -120,19 +116,21 @@ group_test_() ->
           G = my_digraph:create(
                   lists:seq(1, 4), [{1, 2}, {1, 4}, {2, 3}, {3, 4}]),
           GroupedGraph = optimum_supervision_tree_solver:group(G),
-          ?assertEqual(lists:sort([[1, 2, 3], [4]]),
-                       lists:sort(
-                           digraph:vertices(GroupedGraph))),
-          ?assertEqual(lists:sort([{[1, 2, 3], [4]}]), lists:sort(ExtractEdges(GroupedGraph)))
+          ?assertEqual(sort([[3, 2, 1], [4]]), sort(digraph:vertices(GroupedGraph))),
+          % NOTE: Keep the order of `[3, 2, 1]`
+          ?assertEqual([{[3, 2, 1], [4]}],
+                       sort(map(fun(E) ->
+                                   {E, V1, V2, _} = digraph:edge(GroupedGraph, E),
+                                   {V1, V2}
+                                end,
+                                digraph:edges(GroupedGraph))))
        end},
       {"Group each vertex individually if no edges exist",
        fun() ->
           G = my_digraph:create(
                   lists:seq(1, 3), []),
           GroupedGraph = optimum_supervision_tree_solver:group(G),
-          ?assertEqual(lists:sort([[1], [2], [3]]),
-                       lists:sort(
-                           digraph:vertices(GroupedGraph))),
+          ?assertEqual(sort([[1], [2], [3]]), sort(digraph:vertices(GroupedGraph))),
           ?assertEqual([], ExtractEdges(GroupedGraph))
        end},
       {"Label cyclic strongly connected components as `cyclic_strong_component`",
@@ -141,13 +139,13 @@ group_test_() ->
                        digraph:vertex(
                            optimum_supervision_tree_solver:group(
                                my_digraph:create([1, 2], [{1, 2}, {2, 1}])),
-                           [1, 2])),
+                           [2, 1])),
           ?assertMatch({_, cyclic_strong_component},
                        digraph:vertex(
                            optimum_supervision_tree_solver:group(
                                my_digraph:create([1, 2, 3, 4],
                                                  [{1, 2}, {2, 3}, {2, 4}, {3, 1}, {4, 1}])),
-                           [1, 2, 3, 4]))
+                           [4, 3, 2, 1]))
        end},
       {"Does not label singletons as `cyclic_strong_component`",
        fun() ->
@@ -189,10 +187,10 @@ transform_into_optimum_supervision_tree_test_() ->
           GroupedGraph =
               my_digraph:create([[1], [2], [3], [4]], [{[1], [4]}, {[2], [4]}, {[3], [4]}]),
           ?assertEqual({rest_for_one,
-                        [{one_for_one,
+                        [4,
+                         {one_for_one,
                           % NOTE: Not optimal yet.
-                          [{rest_for_one, [3]}, {rest_for_one, [2]}, {rest_for_one, [1]}]},
-                         4]},
+                          [{rest_for_one, [3]}, {rest_for_one, [2]}, {rest_for_one, [1]}]}]},
                        optimum_supervision_tree_solver:transform_into_optimum_supervision_tree(GroupedGraph))
        end},
       {"Interdependent behaviors are monitored by `one_for_all`.",
@@ -205,6 +203,6 @@ transform_into_optimum_supervision_tree_test_() ->
       {"`GroupedVertex` with an in-degree of 0 and a length greater than or equal to 2 is monitored by `rest_for_one`.",
        fun() ->
           GroupedGraph = my_digraph:create([[1, 2, 3], [4]], [{[1, 2, 3], [4]}]),
-          ?assertEqual({rest_for_one, [{rest_for_one, [1, 2, 3]}, 4]},
+          ?assertEqual({rest_for_one, [4, {rest_for_one, [1, 2, 3]}]},
                        optimum_supervision_tree_solver:transform_into_optimum_supervision_tree(GroupedGraph))
        end}]}.
