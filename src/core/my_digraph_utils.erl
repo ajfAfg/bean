348cd738fc8b987a89732d5ed5b9e0a2243ac906
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2023-06-03 13:51:29 +0900

    remove unneeded functions, modules and its tests

--- src/core/my_digraph_utils.erl
+++ src/core/my_digraph_utils.erl
@@ -1,27 +1,6 @@
 -module(my_digraph_utils).
 
--export([get_strong_component/2, get_cyclic_strong_component/2, clone/1]).
-
--spec get_strong_component(digraph:graph(), digraph:vertex()) ->
-                              [digraph:vertex()] | false.
-get_strong_component(Digraph, Vertex) ->
-    StrongComponents = digraph_utils:strong_components(Digraph),
-    case lists:filter(fun(Component) -> lists:member(Vertex, Component) end, StrongComponents)
-    of
-        [C] -> C;
-        _ -> false
-    end.
-
--spec get_cyclic_strong_component(digraph:graph(), digraph:vertex()) ->
-                                     [digraph:vertex()] | false.
-get_cyclic_strong_component(Digraph, Vertex) ->
-    CyclicStrongComponents = digraph_utils:cyclic_strong_components(Digraph),
-    case lists:filter(fun(Component) -> lists:member(Vertex, Component) end,
-                      CyclicStrongComponents)
-    of
-        [C] -> C;
-        _ -> false
-    end.
+-export([clone/1]).
 
 -spec clone(digraph:graph()) -> digraph:graph().
 clone(Digraph) -> digraph_utils:subgraph(Digraph, digraph:vertices(Digraph)).
--- test/prop_my_digraph_utils.erl
+++ test/prop_my_digraph_utils.erl
@@ -21,50 +21,6 @@ create_edges_randomly(Vertices) ->
     Vs = fun() -> my_lists:sublist_randomly(Vertices) end,
     [{V, U} || V <- Vs(), U <- Vs()].
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% my_digraph_utils:get_strong_component/2 %%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-prop_get_strong_component(doc) ->
-    "If the return value is a list, it satisfies the conditions of a strongly connected component".
-
-prop_get_strong_component() ->
-    ?FORALL({Vertices, Edges},
-            vertices_and_edges(),
-            begin
-                G = my_digraph:create(Vertices, Edges),
-                ?FORALL(Vertex,
-                        exactly(hd(my_lists:shuffle(Vertices))),
-                        begin
-                            SCC = my_digraph_utils:get_strong_component(G, Vertex),
-                            ?IMPLIES(is_list(SCC),
-                                     lists:all(fun ({V, V}) -> true;
-                                                   ({V1, V2}) -> my_digraph:has_path(G, V1, V2)
-                                               end,
-                                               [{V1, V2} || V1 <- SCC, V2 <- SCC]))
-                        end)
-            end).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% my_digraph_utils:get_cyclic_strong_component/2 %%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-prop_get_cyclic_strong_component(doc) ->
-    "If the return value is a list, it satisfies the conditions of a cyclic strongly connected component".
-
-prop_get_cyclic_strong_component() ->
-    ?FORALL({Vertices, Edges},
-            vertices_and_edges(),
-            begin
-                G = my_digraph:create(Vertices, Edges),
-                ?FORALL(Vertex,
-                        exactly(hd(my_lists:shuffle(Vertices))),
-                        begin
-                            CSCC = my_digraph_utils:get_cyclic_strong_component(G, Vertex),
-                            ?IMPLIES(is_list(CSCC),
-                                     lists:all(fun({V1, V2}) -> my_digraph:has_path(G, V1, V2) end,
-                                               [{V1, V2} || V1 <- CSCC, V2 <- CSCC]))
-                        end)
-            end).
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% my_digraph_utils:clone/1 %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
