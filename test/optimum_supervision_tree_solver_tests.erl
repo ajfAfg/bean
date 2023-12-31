89b4b251c428fd6e89105ce73c9f85e8ebd7f2b9
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2023-12-31 19:15:18 +0900

    Pass tests if the result of `optimum_supervision_tree_solver:solve/1` is isomorphic to one of the expected optimal supervision trees

--- test/optimum_supervision_tree_solver_tests.erl
+++ test/optimum_supervision_tree_solver_tests.erl
@@ -4,17 +4,24 @@
 
 -import(lists, [map/2, sort/1]).
 
--define(assertIso(T1, T2),
+-define(assertExistsIsoTree(ExpectedTrees, ActualTree),
         % NOTE:
         % If a user-defined function is used to determine if two values are equal,
         % the got value is not displayed when the test fails.
         % Therefore, the expected value and got value are explicitly displayed
         % (though the display method is slightly distorted from the usual).
         begin
-            V = supervision_tree:are_isomorphic(T1, T2),
+            V = lists:any(fun(ExpectedTree) ->
+                             supervision_tree:are_isomorphic(ExpectedTree, ActualTree)
+                          end,
+                          ExpectedTrees),
             case V of
                 true -> ok;
-                false -> ?debugFmt("~nexpected: ~p~ngot: ~p~n", [T1, T2])
+                false ->
+                    ?debugFmt("~nexpected: ~s~nactual: ~p~n",
+                              [string:join([io_lib:format("~p", [T]) || T <- ExpectedTrees],
+                                           " or\n          "),
+                               ActualTree])
             end,
             ?assert(V)
         end).
@@ -34,9 +41,13 @@ solve_test_() ->
        fun() ->
           G = CreateGraph(seq_atom(1, 7),
                           [{'1', '2'}, {'2', '3'}, {'3', '4'}, {'3', '5'}, {'6', '7'}]),
-          ?assertIso({one_for_one,
-                      [{rest_for_one, ['5', '4', '3', '2', '1']}, {rest_for_one, ['7', '6']}]},
-                     optimum_supervision_tree_solver:solve(G))
+          ?assertExistsIsoTree([{one_for_one,
+                                 [{rest_for_one, ['5', '4', '3', '2', '1']},
+                                  {rest_for_one, ['7', '6']}]},
+                                {one_for_one,
+                                 [{rest_for_one, ['4', '5', '3', '2', '1']},
+                                  {rest_for_one, ['7', '6']}]}],
+                               optimum_supervision_tree_solver:solve(G))
        end},
       {"Graph like Lattice",
        fun() ->
@@ -48,29 +59,31 @@ solve_test_() ->
                            {'3', '6'},
                            {'4', '6'},
                            {'5', '6'}]),
-          ?assertIso({rest_for_one,
-                      ['6',
-                       '4',
-                       {one_for_one, [{rest_for_one, ['3', '1']}, {rest_for_one, ['5', '2']}]}]},
-                     optimum_supervision_tree_solver:solve(G))
+          ?assertExistsIsoTree([{rest_for_one,
+                                 ['6',
+                                  '4',
+                                  {one_for_one,
+                                   [{rest_for_one, ['3', '1']}, {rest_for_one, ['5', '2']}]}]}],
+                               optimum_supervision_tree_solver:solve(G))
        end},
       {"Binary tree",
        fun() ->
           G = CreateGraph(seq_atom(1, 7),
                           [{'1', '3'}, {'2', '3'}, {'3', '7'}, {'4', '6'}, {'5', '6'}, {'6', '7'}]),
-          ?assertIso({rest_for_one,
-                      ['7',
-                       {one_for_one,
-                        [{rest_for_one, ['3', {one_for_one, ['1', '2']}]},
-                         {rest_for_one, ['6', {one_for_one, ['4', '5']}]}]}]},
-                     optimum_supervision_tree_solver:solve(G))
+          ?assertExistsIsoTree([{rest_for_one,
+                                 ['7',
+                                  {one_for_one,
+                                   [{rest_for_one, ['3', {one_for_one, ['1', '2']}]},
+                                    {rest_for_one, ['6', {one_for_one, ['4', '5']}]}]}]}],
+                               optimum_supervision_tree_solver:solve(G))
        end},
       {"Graph having circles",
        fun() ->
           G1 = CreateGraph(seq_atom(1, 5),
                            [{'1', '2'}, {'2', '3'}, {'3', '4'}, {'4', '2'}, {'3', '5'}]),
-          ?assertIso({rest_for_one, ['5', {one_for_all, ['4', '3', '2', {rest_for_one, ['1']}]}]},
-                     optimum_supervision_tree_solver:solve(G1)),
+          ?assertExistsIsoTree([{rest_for_one,
+                                 ['5', {one_for_all, ['4', '3', '2', {rest_for_one, ['1']}]}]}],
+                               optimum_supervision_tree_solver:solve(G1)),
           G2 = CreateGraph(seq_atom(1, 5),
                            [{'1', '2'},
                             {'2', '3'},
@@ -78,12 +91,12 @@ solve_test_() ->
                             {'2', '5'},
                             {'3', '1'},
                             {'4', '5'}]),
-          ?assertIso({rest_for_one, ['5', '4', {one_for_all, ['3', '2', '1']}]},
-                     optimum_supervision_tree_solver:solve(G2)),
+          ?assertExistsIsoTree([{rest_for_one, ['5', '4', {one_for_all, ['3', '2', '1']}]}],
+                               optimum_supervision_tree_solver:solve(G2)),
           G3 = CreateGraph(seq_atom(1, 4),
                            [{'1', '2'}, {'2', '1'}, {'2', '3'}, {'3', '4'}, {'4', '3'}]),
-          ?assertIso({one_for_all, ['3', '4', {one_for_all, ['1', '2']}]},
-                     optimum_supervision_tree_solver:solve(G3)),
+          ?assertExistsIsoTree([{one_for_all, ['3', '4', {one_for_all, ['1', '2']}]}],
+                               optimum_supervision_tree_solver:solve(G3)),
           G4 = CreateGraph(seq_atom(1, 8),
                            [{'1', '2'},
                             {'2', '1'},
@@ -94,13 +107,15 @@ solve_test_() ->
                             {'6', '5'},
                             {'6', '8'},
                             {'7', '8'}]),
-          ?assertIso({rest_for_one,
-                      ['8',
-                       {one_for_one,
-                        ['7',
-                         {one_for_all,
-                          ['5', '6', {rest_for_one, ['4', '3', {one_for_all, ['1', '2']}]}]}]}]},
-                     optimum_supervision_tree_solver:solve(G4)),
+          ?assertExistsIsoTree([{rest_for_one,
+                                 ['8',
+                                  {one_for_one,
+                                   ['7',
+                                    {one_for_all,
+                                     ['5',
+                                      '6',
+                                      {rest_for_one, ['4', '3', {one_for_all, ['1', '2']}]}]}]}]}],
+                               optimum_supervision_tree_solver:solve(G4)),
           G5 = CreateGraph(seq_atom(1, 6),
                            [{'1', '3'},
                             {'2', '3'},
@@ -108,11 +123,11 @@ solve_test_() ->
                             {'4', '5'},
                             {'5', '4'},
                             {'5', '6'}]),
-          ?assertIso({rest_for_one,
-                      ['6',
-                       {one_for_all,
-                        ['5', '4', {rest_for_one, ['3', {one_for_one, ['2', '1']}]}]}]},
-                     optimum_supervision_tree_solver:solve(G5))
+          ?assertExistsIsoTree([{rest_for_one,
+                                 ['6',
+                                  {one_for_all,
+                                   ['5', '4', {rest_for_one, ['3', {one_for_one, ['2', '1']}]}]}]}],
+                               optimum_supervision_tree_solver:solve(G5))
        end},
       {"Graph having circles in first and last",
        fun() ->
@@ -124,25 +139,30 @@ solve_test_() ->
                            {'4', '5'},
                            {'5', '6'},
                            {'6', '5'}]),
-          ?assertIso({one_for_all,
-                      ['6', '5', {rest_for_one, ['4', {one_for_all, ['3', '2', '1']}]}]},
-                     optimum_supervision_tree_solver:solve(G))
+          ?assertExistsIsoTree([{one_for_all,
+                                 ['6',
+                                  '5',
+                                  {rest_for_one, ['4', {one_for_all, ['3', '2', '1']}]}]}],
+                               optimum_supervision_tree_solver:solve(G))
        end},
       {"Cycle graph",
        fun() ->
           G = CreateGraph(seq_atom(1, 4),
                           [{'1', '2'}, {'2', '3'}, {'2', '4'}, {'3', '1'}, {'4', '1'}]),
-          ?assertIso({one_for_all, ['3', '4', '2', '1']}, optimum_supervision_tree_solver:solve(G))
+          ?assertExistsIsoTree([{one_for_all, ['3', '4', '2', '1']}],
+                               optimum_supervision_tree_solver:solve(G))
        end},
       {"Graph with detour",
        fun() ->
           G = CreateGraph(seq_atom(1, 4), [{'1', '2'}, {'1', '4'}, {'2', '3'}, {'3', '4'}]),
-          ?assertIso({rest_for_one, ['4', '3', '2', '1']}, optimum_supervision_tree_solver:solve(G))
+          ?assertExistsIsoTree([{rest_for_one, ['4', '3', '2', '1']}],
+                               optimum_supervision_tree_solver:solve(G))
        end},
       {"Null graph",
        fun() ->
           G = CreateGraph(seq_atom(1, 3), []),
-          ?assertIso({one_for_one, ['1', '2', '3']}, optimum_supervision_tree_solver:solve(G))
+          ?assertExistsIsoTree([{one_for_one, ['1', '2', '3']}],
+                               optimum_supervision_tree_solver:solve(G))
        end},
       {"Graph where vertices in the split vertices are not necessarily connected to each other",
        fun() ->
@@ -156,9 +176,17 @@ solve_test_() ->
                            {'4', '5'},
                            {'4', '7'},
                            {'5', '6'}]),
-          ?assertIso({rest_for_one,
-                      ['6', '5', '3', {one_for_one, ['1', {rest_for_one, ['7', '4', '2']}]}]},
-                     optimum_supervision_tree_solver:solve(G))
+          ?assertExistsIsoTree([{rest_for_one,
+                                 ['6',
+                                  '5',
+                                  '3',
+                                  {one_for_one, ['1', {rest_for_one, ['7', '4', '2']}]}]},
+                                {rest_for_one,
+                                 ['3',
+                                  '6',
+                                  '5',
+                                  {one_for_one, ['1', {rest_for_one, ['7', '4', '2']}]}]}],
+                               optimum_supervision_tree_solver:solve(G))
        end}]}.
 
 take_split_vertices_test_() ->
