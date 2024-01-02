b6dd048e05c772df85ff52850f0ffa0e8b00284c
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2024-01-02 11:52:16 +0900

    Change to use the new function solving all local minimum vertex splitters

--- src/core/optimum_supervision_tree_solver.erl
+++ src/core/optimum_supervision_tree_solver.erl
@@ -1,36 +1,44 @@
 -module(optimum_supervision_tree_solver).
 
--export([solve/1, take_all_local_minimum_vertex_splitters/1]).
+-export([solve/2]).
 
 -type dag() :: digraph:graph().
 -type dag_vertex() :: [dependency_digraph:vertex()].
 -type connected_dag() :: digraph:graph().
 -type connected_dag_vertex() :: [dependency_digraph:vertex()].
 
--spec solve(dependency_graph:t()) -> supervision_tree:t().
-solve(DependencyGraph) ->
+-spec solve(dependency_graph:t(),
+            all_local_minimum_vertex_splitters_solver:take_all_local_minimum_vertex_splitters()) ->
+               supervision_tree:t().
+solve(DependencyGraph, TakeAllLocalMinimumVertexSplitters) ->
     Graph = dependency_digraph:from_dependency_graph(DependencyGraph),
-    transform(digraph_utils:condensation(Graph)).
+    transform(digraph_utils:condensation(Graph), TakeAllLocalMinimumVertexSplitters).
 
 % NOTE: The argument graph is assumed to be a DAG.
--spec transform(dag()) -> supervision_tree:t().
-transform(DAG) ->
+-spec transform(dag(),
+                all_local_minimum_vertex_splitters_solver:take_all_local_minimum_vertex_splitters()) ->
+                   supervision_tree:t().
+transform(DAG, TakeAllLocalMinimumVertexSplitters) ->
     case digraph_utils:components(DAG) of
         [] -> throw(impossible);
-        [_] -> transform_(DAG);
+        [_] -> transform_(DAG, TakeAllLocalMinimumVertexSplitters);
         Components ->
             % NOTE: Remove an unneeded supervisor
             Fun = fun ({rest_for_one, [V]}) -> V;
                       (Other) -> Other
                   end,
             {one_for_one,
-             lists:map(fun(Component) -> Fun(transform_(digraph_utils:subgraph(DAG, Component)))
+             lists:map(fun(Component) ->
+                          Fun(transform_(digraph_utils:subgraph(DAG, Component),
+                                         TakeAllLocalMinimumVertexSplitters))
                        end,
                        Components)}
     end.
 
--spec transform_(connected_dag()) -> supervision_tree:t().
-transform_(ConnectedDAG) ->
+-spec transform_(connected_dag(),
+                 all_local_minimum_vertex_splitters_solver:take_all_local_minimum_vertex_splitters()) ->
+                    supervision_tree:t().
+transform_(ConnectedDAG, TakeAllLocalMinimumVertexSplitters) ->
     Candidates =
         [begin
              NextConnectedDAGs =
@@ -43,43 +51,39 @@ transform_(ConnectedDAG) ->
                  end,
              hd(transform__(lists:reverse(sort_by_topological_ordering(VertexSplitter,
                                                                        ConnectedDAG)),
-                            NextConnectedDAGs))
+                            NextConnectedDAGs,
+                            TakeAllLocalMinimumVertexSplitters))
          end
-         || VertexSplitter <- take_all_local_minimum_vertex_splitters(ConnectedDAG)],
+         || VertexSplitter <- TakeAllLocalMinimumVertexSplitters(ConnectedDAG)],
     hd(lists:sort(fun(Tree1, Tree2) ->
                      supervision_tree:calc_cost(Tree1) =< supervision_tree:calc_cost(Tree2)
                   end,
                   Candidates)).
 
--spec transform__([connected_dag_vertex()], [connected_dag()]) -> [supervision_tree:t()].
-transform__([], []) -> [];
-transform__([], NextConnectedDAGs) ->
+-spec transform__([connected_dag_vertex()],
+                  [connected_dag()],
+                  all_local_minimum_vertex_splitters_solver:take_all_local_minimum_vertex_splitters()) ->
+                     [supervision_tree:t()].
+transform__([], [], _TakeAllLocalMinimumVertexSplitters) -> [];
+transform__([], NextConnectedDAGs, TakeAllLocalMinimumVertexSplitters) ->
     % NOTE: Remove an unneeded supervisor
     Fun = fun ({rest_for_one, [V]}) -> V;
               (Other) -> Other
           end,
-    [{one_for_one, lists:map(Fun, lists:map(fun transform_/1, NextConnectedDAGs))}];
-transform__([C | _] = Components, NextDAGs) when length(C) =:= 1 ->
+    [{one_for_one,
+      lists:map(Fun,
+                lists:map(fun(G) -> transform_(G, TakeAllLocalMinimumVertexSplitters) end,
+                          NextConnectedDAGs))}];
+transform__([C | _] = Components, NextDAGs, TakeAllLocalMinimumVertexSplitters)
+    when length(C) =:= 1 ->
     {Cs1, Cs2} = lists:splitwith(fun(C_) -> length(C_) =:= 1 end, Components),
-    [{rest_for_one, lists:flatten(Cs1) ++ transform__(Cs2, NextDAGs)}];
-transform__([C | Components], NextDAGs) ->
-    [{one_for_all, C ++ transform__(Components, NextDAGs)}].
+    [{rest_for_one,
+      lists:flatten(Cs1) ++ transform__(Cs2, NextDAGs, TakeAllLocalMinimumVertexSplitters)}];
+transform__([C | Components], NextDAGs, TakeAllLocalMinimumVertexSplitters) ->
+    [{one_for_all,
+      C ++ transform__(Components, NextDAGs, TakeAllLocalMinimumVertexSplitters)}].
 
 % NOTE: The argument graph is assumed to be a DAG.
 -spec sort_by_topological_ordering([dag_vertex()], dag()) -> [dag_vertex()].
 sort_by_topological_ordering(Vertices, DAG) ->
     lists:filter(fun(V) -> lists:member(V, Vertices) end, digraph_utils:topsort(DAG)).
-
--spec take_all_local_minimum_vertex_splitters(connected_dag()) ->
-                                                 [[connected_dag_vertex()]].
-take_all_local_minimum_vertex_splitters(ConnectedDAG) ->
-    lists:usort([sets:to_list(
-                     lists:foldl(fun(Vs, Acc) -> sets:intersection(Acc, sets:from_list(Vs)) end,
-                                 sets:from_list(
-                                     digraph:vertices(ConnectedDAG)),
-                                 [digraph_utils:reachable([U], ConnectedDAG)
-                                  || U <- digraph:vertices(ConnectedDAG),
-                                     digraph:in_degree(ConnectedDAG, U) =:= 0,
-                                     my_digraph:has_path(ConnectedDAG, U, V)]))
-                 || V <- digraph:vertices(ConnectedDAG),
-                    digraph:out_degree(ConnectedDAG, V) =:= 0]).
--- test/prop_optimum_supervision_tree_solver.erl
+++ test/prop_optimum_supervision_tree_solver.erl
@@ -6,28 +6,6 @@
 
 -include_lib("proper/include/proper.hrl").
 
-dependency_graph() ->
-    ?LET(List,
-         non_empty(list(limited_atom())),
-         begin
-             Vertices = lists:uniq(List),
-             lists:foldl(fun(From, Acc) ->
-                            Tos = my_lists:sublist_randomly(Vertices),
-                            maps:put(From, Tos, Acc)
-                         end,
-                         #{},
-                         Vertices)
-         end).
-
-dependency_graph_to_connected_dag(G) ->
-    Digraph =
-        digraph_utils:condensation(
-            dependency_digraph:from_dependency_graph(G)),
-    digraph_utils:subgraph(Digraph, hd(digraph_utils:components(Digraph))).
-
-dependency_connected_dag() ->
-    {'$call', ?MODULE, dependency_graph_to_connected_dag, [dependency_graph()]}.
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% optimum_supervision_tree_solver:solve/1 %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -36,13 +14,14 @@ prop_solve(doc) ->
 
 prop_solve() ->
     ?FORALL(Graph,
-            dependency_graph(),
+            dependency_graph_generator:dependency_graph(),
             begin
                 Digraph = dependency_digraph:from_dependency_graph(Graph),
                 lists:all(fun(V) ->
                              digraph_utils:reaching([V], Digraph)
                              -- take_restart_processes(V,
-                                                       optimum_supervision_tree_solver:solve(Graph))
+                                                       optimum_supervision_tree_solver:solve(Graph,
+                                                                                             fun all_local_minimum_vertex_splitters_solver:solve_in_polynomial_time_without_correctness/1))
                              =:= []
                           end,
                           digraph:vertices(Digraph))
@@ -69,45 +48,3 @@ take_restart_processes({_, Children}) ->
     lists:flatten(
         lists:map(fun take_restart_processes/1, Children));
 take_restart_processes(Child) -> Child.
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% optimum_supervision_tree_solver:take_all_local_minimum_vertex_splitters/1 %%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-prop_take_all_local_minimum_vertex_splitters1(doc) ->
-    "Satisfy constraint 1 of the vertex splitter".
-
-prop_take_all_local_minimum_vertex_splitters1() ->
-    ?FORALL(ConnectedDAG,
-            dependency_connected_dag(),
-            begin
-                lists:all(fun(X) -> X end,
-                          [lists:member(U, VertexSplitter)
-                           || VertexSplitter
-                                  <- optimum_supervision_tree_solver:take_all_local_minimum_vertex_splitters(ConnectedDAG),
-                              V <- VertexSplitter,
-                              U <- digraph_utils:reachable([V], ConnectedDAG)])
-            end).
-
-% TODO:
-% The current implementation do not always satisfy Vertex splitter property 2.
-
-% prop_take_all_local_minimum_vertex_splitters2(doc) ->
-%     "Satisfy constraint 2 of the vertex splitter".
-%
-% prop_take_all_local_minimum_vertex_splitters2() ->
-%     ?FORALL(ConnectedDAG,
-%             dependency_connected_dag(),
-%             begin
-%                 lists:all(fun(X) -> X end,
-%                           [not
-%                                (lists:sort(
-%                                     digraph:vertices(ConnectedDAG))
-%                                 =/= lists:sort(VertexSplitter))
-%                            orelse length(digraph_utils:components(
-%                                              digraph_utils:subgraph(
-%                                                  my_digraph_utils:clone(ConnectedDAG),
-%                                                  digraph:vertices(ConnectedDAG) -- VertexSplitter)))
-%                                   >= 2
-%                            || VertexSplitter
-%                                   <- optimum_supervision_tree_solver:take_all_local_minimum_vertex_splitters(ConnectedDAG)])
-%             end).
