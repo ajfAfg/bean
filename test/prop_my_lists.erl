a9b7ba513d8f867547caea373185209dfc49a5cf
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2024-01-02 11:49:06 +0900

    Add a function to solve a power list (like power set) and its tests

--- test/prop_my_lists.erl
+++ test/prop_my_lists.erl
@@ -41,3 +41,22 @@ prop_sublist_randomly2(doc) ->
 
 prop_sublist_randomly2() ->
     ?FORALL(List, list(random_type()), my_lists:sublist_randomly(List) -- List =:= []).
+
+%%%%%%%%%%%%%%%%%%%%%%%%
+%%% my_lists:power/1 %%%
+%%%%%%%%%%%%%%%%%%%%%%%%
+prop_power1(doc) ->
+    "Let n be the length of the original list, the length of the return value is 2^n".
+
+prop_power1() ->
+    ?FORALL(List,
+            ?SUCHTHAT(L, list(random_type()), length(L) =< 20),
+            length(my_lists:power(List)) =:= trunc(math:pow(2, length(List)))).
+
+prop_power2(doc) -> "The element of the return value is a sublist of the original list".
+
+prop_power2() ->
+    ?FORALL(List,
+            ?SUCHTHAT(L, list(random_type()), length(L) =< 15),
+            lists:all(fun(List2) -> lists:sort(List2 ++ List -- List2) =:= lists:sort(List) end,
+                      my_lists:power(List))).
