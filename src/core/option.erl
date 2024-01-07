c0f15966d55f5914075103cd7728d207b8024577
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2024-01-07 12:06:55 +0900

    Add the function to get a value from the option value

--- src/core/option.erl
+++ src/core/option.erl
@@ -1,5 +1,11 @@
 -module(option).
 
+-export([get/1]).
+
 -export_type([t/1]).
 
 -type t(T) :: {some, T} | none.
+
+% NOTE: No support for `none`
+-spec get(t(T)) -> T when T :: term().
+get({some, V}) -> V.
