9ad98d3ce54b36957c6ee4fd83797735a946e7d3
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2024-01-14 17:52:56 +0900

    Update the parameters

--- benchmarks/measure_execution_time_of_function_that_solve_optimal_supervision_tree/src/parameters.erl
+++ benchmarks/measure_execution_time_of_function_that_solve_optimal_supervision_tree/src/parameters.erl
@@ -4,7 +4,7 @@
 
 vertex_num_and_edge_num_list() ->
     [{VertexNum, EdgeNum}
-     || VertexNum <- lists:seq(10, 70, 10),
-        EdgeNum <- lists:map(fun(X) -> VertexNum * X end, [1, 2, 3, VertexNum - 1])].
+     || VertexNum <- lists:seq(10, 80, 10),
+        EdgeNum <- lists:map(fun(X) -> VertexNum * X end, [1, 2, 3])].
 
 trial_num() -> 20.
